2a0e5ef7867c3cf25e7eb1b493e2b9da8490f598
soulard <soulard@cubecom.fr>
2015-03-02 15:33:19 +0100

    Mise à jour version 2.2.0

---
+++ images/index.php
@@ -0,0 +1,33 @@
+<?php
+/**
+* NOTICE OF LICENSE
+*
+* This source file is subject to a commercial license from Adonie SAS - Ecopresto
+* Use, copy, modification or distribution of this source file without written
+* license agreement from Adonie SAS is strictly forbidden.
+* In order to obtain a license, please contact us: info@ecopresto.com
+* ...........................................................................
+* INFORMATION SUR LA LICENCE D'UTILISATION
+*
+* L'utilisation de ce fichier source est soumise a une licence commerciale
+* concedee par la societe Adonie SAS - Ecopresto
+* Toute utilisation, reproduction, modification ou distribution du present
+* fichier source sans contrat de licence ecrit de la part de la SAS Adonie - Ecopresto est
+* expressement interdite.
+* Pour obtenir une licence, veuillez contacter Adonie SAS a l'adresse: info@ecopresto.com
+* ...........................................................................
+*
+*  @package ec_ecopresto
+*  @author Adonie SAS - Ecopresto
+*  @version 2.2
+*  @copyright Copyright (c) Adonie SAS
+*  @license Commercial license
+*/
+
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', false);
+header('Pragma: no-cache');
+header('Location: ../');
+exit;
