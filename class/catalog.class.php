290648bf428dd956fab76603ea036c6fe913a52f
soulard <soulard@cubecom.fr>
2015-03-03 10:00:55 +0100

    Gestion des erreurs SQL + Fonction update v220 manuelle

--- class/catalog.class.php
+++ class/catalog.class.php
@@ -969,9 +969,9 @@ class Catalog
 		if ((int)$nbligne > 100000 || (int)$nbligne == 0) {
 			$nbligne = 2000;
 		}
-		//chercher le nombre de ligne restant à traiter
-		$tabEtat = $this->etatCatalogBrutToEcopresto();
 		
+		//chercher le nombre de ligne restant à traiter
+		$tabEtat = $this->etatCatalogBrutToEcopresto(&$tabErreur);
 		//Si premier passage, supprimer les données présentes dans le catalogue actuel
 		if ($tabEtat[0] == $tabEtat[2]) {
 			$this->deleteData();
@@ -1026,7 +1026,7 @@ class Catalog
 			'".pSQL($data['weight'])."' ,
 			'".pSQL($data['pmvc'])."');";
 			if (!Db::getInstance(_PS_USE_SQL_SLAVE_)->execute($sql_catalog))
-				return false;
+				$tabErreur[] = 'SQL Catalog: '.$sql_catalog;
 				
 			if ($data['reference_attribute'] != '' && $data['reference'] != $data['reference_attribute']) {
 				//Requête pour insertion dans la table attributs :
@@ -1043,12 +1043,12 @@ class Catalog
 				'".pSQL($data['attribute_4'])."',  
 				'".pSQL($data['attribute_5'])."');";
 				if (!Db::getInstance(_PS_USE_SQL_SLAVE_)->execute($sql_attribute))
-					return false;
+					$tabErreur[] = 'SQL Attribute: '.$sql_attribute;
 			}
 			
 			$sql_updatecataloguebrut = 'UPDATE `'._DB_PREFIX_.'ec_ecopresto_cataloguebrut` SET importbrut=1 WHERE `reference_attribute`="'.pSQL($data['reference_attribute']).'"';
 			if (!Db::getInstance(_PS_USE_SQL_SLAVE_)->execute($sql_updatecataloguebrut))
-					return false;
+				$tabErreur[] = 'SQL Update Catalog Brut: '.$sql_updatecataloguebrut;
 			
 			//Construire la liste des attributs : 
 			if ($data['attribute_1'] != '')
@@ -1173,5 +1173,65 @@ class Catalog
 	public function UpdateDereferencement($ref)
 	{
 		Db::getInstance()->execute('UPDATE `'._DB_PREFIX_.'ec_ecopresto_product_deleted` SET `status`= 1 WHERE `reference`="'.pSQL($ref).'"');
+	}
+		/**
+	 * Créé la table catalogbrut et ajoute une ligne à la conf
+	 *
+	 * @param aucun
+	 * @return boolean false si erreur
+	 */
+	public function creer_table_v220() {
+		$sql_v220 = 'CREATE TABLE IF NOT EXISTS `'._DB_PREFIX_.'ec_ecopresto_cataloguebrut` (
+		`category_1` varchar(255) NOT NULL,
+		`category_2` varchar(255) NOT NULL,
+		`category_3` varchar(255) NOT NULL,
+		`category_4` varchar(255) NOT NULL,
+		`category_5` varchar(255) NOT NULL,
+		`ss_category_1` varchar(255) NOT NULL,
+		`ss_category_2` varchar(255) NOT NULL,
+		`ss_category_3` varchar(255) NOT NULL,
+		`ss_category_4` varchar(255) NOT NULL,
+		`ss_category_5` varchar(255) NOT NULL,
+		`reference` varchar(255) NOT NULL,
+		`reference_attribute` varchar(255) NOT NULL,
+		`manufacturer` varchar(255) NOT NULL,
+		`attribute_1` text NOT NULL,
+		`attribute_2` text NOT NULL,
+		`attribute_3` text NOT NULL,
+		`attribute_4` text NOT NULL,
+		`attribute_5` text NOT NULL,
+		`name_1` varchar(255) NOT NULL,
+		`name_2` varchar(255) NOT NULL,
+		`name_3` varchar(255) NOT NULL,
+		`name_4` varchar(255) NOT NULL,
+		`name_5` varchar(255) NOT NULL,
+		`description_short_1` text NOT NULL,
+		`description_short_2` text NOT NULL,
+		`description_short_3` text NOT NULL,
+		`description_short_4` text NOT NULL,
+		`description_short_5` text NOT NULL,
+		`description_1` text NOT NULL,
+		`description_2` text NOT NULL,
+		`description_3` text NOT NULL,
+		`description_4` text NOT NULL,
+		`description_5` text NOT NULL,
+		`price` varchar(25) NOT NULL,
+		`image_1` text NOT NULL,
+		`image_2` text NOT NULL,
+		`image_3` text NOT NULL,
+		`image_4` text NOT NULL,
+		`image_5` text NOT NULL,
+		`image_6` text NOT NULL,
+		`rate` varchar(25) NOT NULL,
+		`ean13` varchar(25) NOT NULL,
+		`weight` varchar(25) NOT NULL,
+		`pmvc` varchar(25) NOT NULL,
+		`importbrut` tinyint(1) NOT NULL DEFAULT 0
+		);
+		INSERT INTO `PREFIX_ec_ecopresto_configuration`(`name`, `value`, `id_shop`) VALUES (`NB_LIGNE_IMPORT`,5000,1);';
+		if (!Db::getInstance(_PS_USE_SQL_SLAVE_)->execute($sql_v220))
+			return = 'SQL creer_table_v220: '.$sql_v220;
+		else
+			return true;
 	}
 }
