159fd5c0171c16046fe76b1eff967532509ea974
soulard <soulard@cubecom.fr>
2015-03-03 10:22:38 +0100

    Ajout fonction check doublon CSV

--- class/catalog.class.php
+++ class/catalog.class.php
@@ -1174,7 +1174,7 @@ class Catalog
 	{
 		Db::getInstance()->execute('UPDATE `'._DB_PREFIX_.'ec_ecopresto_product_deleted` SET `status`= 1 WHERE `reference`="'.pSQL($ref).'"');
 	}
-		/**
+	/**
 	 * Créé la table catalogbrut et ajoute une ligne à la conf
 	 *
 	 * @param aucun
@@ -1228,10 +1228,24 @@ class Catalog
 		`pmvc` varchar(25) NOT NULL,
 		`importbrut` tinyint(1) NOT NULL DEFAULT 0
 		);
-		INSERT INTO `PREFIX_ec_ecopresto_configuration`(`name`, `value`, `id_shop`) VALUES (`NB_LIGNE_IMPORT`,5000,1);';
+		DELETE FROM `'._DB_PREFIX_.'ec_ecopresto_configuration` WHERE `name`="NB_LIGNE_IMPORT";
+		INSERT INTO `'._DB_PREFIX_.'ec_ecopresto_configuration`(`name`, `value`, `id_shop`) VALUES (`NB_LIGNE_IMPORT`,5000,1);';
 		if (!Db::getInstance(_PS_USE_SQL_SLAVE_)->execute($sql_v220))
-			return = 'SQL creer_table_v220: '.$sql_v220;
+			return = 'Erreur SQL creer_table_v220: '.$sql_v220;
 		else
 			return true;
+	}
+	/**
+	 * Créé la table catalogbrut et ajoute une ligne à la conf
+	 *
+	 * @param aucun
+	 * @return boolean false si erreur
+	 */
+	public function check_doublon_csv(){
+		$sql_doublon = 'SELECT   COUNT(reference_attribute) AS nb_doublon, reference_attribute
+			FROM `'._DB_PREFIX_.'ec_ecopresto_cataloguebrut` 
+			GROUP BY reference_attribute
+			HAVING COUNT(reference_attribute) > 1';
+		return Db::getInstance(_PS_USE_SQL_SLAVE_)->execute($sql_doublon);
 	}
 }
